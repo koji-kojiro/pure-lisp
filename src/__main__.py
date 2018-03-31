27ba0492416d1117e69a3fb13472a6b1a37bfc45
TANI Kojiro <kojiro0531@gmail.com>
2018-03-31 18:23:43 +0900

    Improve command line

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,40 @@
 import sys
-from repl import repl
+import pkgutil
+import argparse
+from read import read
 from eval import eval_sexp
-from read import read, peek_char, delimiters
+from repl import repl, Repl
 
-if len(sys.argv) == 1:
-    sexp = read(eof_error_p=False)
-    while(sexp):
-        eval_sexp(sexp)
-        sexp = read(eof_error_p=False)
-elif "--repl" in sys.argv:
-    repl()
+def list_extensions():
+    import extensions
+    modules = [info.name for info in  pkgutil.iter_modules(extensions.__path__)]
+    return [module for module in modules if not module.startswith("_")]
+
+def enable_extensions(*args):
+    extensions = list(args) if args else list_extensions()
+    __import__("extensions", fromlist=extensions)
+    Repl.intro += f"[Enabled extensions: {extensions}]\n"
+
+def run_source(fname):
+    with open(fname, "r") as f:
+        sexp = read(f, eof_error_p=False)
+        while sexp:
+            eval_sexp(sexp)
+            sexp = read(f, eof_error_p=False)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="lisp source code.", nargs="?")
+parser.add_argument("-e", "--ext", help="enable extensions.", nargs="*", metavar="ext", choices=list_extensions())
+parser.add_argument("--list-ext", help="list avilable extensions.", action="store_true")
+args = parser.parse_args()
+
+if args.list_ext:
+    sys.exit("\n".join(list_extensions()))
+
+if args.ext is not None:
+    enable_extensions(*args.ext)
+
+if args.file:
+    run_source(args.file)
 else:
-    sys.exit("usage: {} [--repl]".format(sys.argv[0]))
+    repl()
