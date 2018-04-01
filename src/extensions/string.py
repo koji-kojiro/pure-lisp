e451622fb60f576830b17f7e9bca9a542d86bc4d
TANI Kojiro <kojiro0531@gmail.com>
2018-04-01 18:53:54 +0900

    Add `string` extension

---
+++ src/extensions/string.py
@@ -0,0 +1,43 @@
+from . import _import
+import read
+import builtin
+
+class LispString(str):
+    def __eq__(self, other):
+        if isinstance(other, LispString):
+           return str(self) == str(other)
+        else:
+           return False
+
+    def upper(self):
+        return LispString(str.upper(self))
+
+    def lower(self):
+        return LispString(str.lower(self))
+   
+    def capitalize(self):
+        return LispString(str.capitalize(self))
+
+special_forms = {
+    "string": lambda string, env, hook: LispString(string),
+}
+
+functions = {
+    "readline": lambda : LispString(input()),
+    "string-upcase": LispString.upper,
+    "string-downcase": LispString.lower,
+    "string-capitalize": LispString.capitalize,
+}
+
+builtin.special_forms.update(special_forms)
+builtin.functions.update(functions)
+
+def read_string(stream):
+    string = ""
+    char = read.read_char(stream)
+    while char is not "\"":
+        string += char
+        char = read.read_char(stream)
+    return ["string", string]
+
+read.readtable.update({"\"": read_string})
