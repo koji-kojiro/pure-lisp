bc4b3ff729fd8bb0b73f1ccd3df9985ec591b621
TANI Kojiro <kojiro0531@gmail.com>
2018-04-01 23:53:02 +0900

    Fix pprint

--- src/pprint.py
+++ src/pprint.py
@@ -1,15 +1,16 @@
 from builtin import functions
 
 def pprint(value, end="\n"):
-    if not value:
-        print("nil", end=end)
-    elif value is True:
+    if value is True:
         print("t", end=end)
     elif type(value) is list:
-        print("(", end="")
-        for element in value[:-1]:
-            pprint(element, end=" ")
-        pprint(value[-1], end=")\n")
+        if not value:
+            print("nil", end=end)
+        else:
+            print("(", end="")
+            for element in value[:-1]:
+                pprint(element, end=" ")
+            pprint(value[-1], end=")\n")
     else:
         print(value, end=end)
     return "nil"
